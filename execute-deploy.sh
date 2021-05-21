f8291fe11be94497b87497e4a35c283c56091855
dotoryeee <bayer@kakao.com>
2021-05-22 01:13:57 +0900

    fix: gunicorn + nginx

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo kill $(pgrep -f python3)
-# python3 /home/ec2-user/app/app.py &
-# cd home/ec2-user/app/
-# gunicorn -w 1 app:app
-# systemctl restart nginx
+cd /home/ec2-user/app/
+systemctl restart nginx
+gunicorn -w 1 app:app
